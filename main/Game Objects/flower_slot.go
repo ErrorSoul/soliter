467cbf5d7d11105c0c249c870a134526d299e0b3
Abu Sultanov <neurofag@gmail.com>
2025-02-12 09:36:12 +0300

    refactor position and flower slot

---
+++ main/Game Objects/flower_slot.go
@@ -0,0 +1,23 @@
+components {
+  id: "base_slot"
+  component: "/main/Scripts/base_slot.script"
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"back\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "size {\n"
+  "  x: 90.0\n"
+  "  y: 150.0\n"
+  "}\n"
+  "size_mode: SIZE_MODE_MANUAL\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/main.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    z: -0.2
+  }
+}
