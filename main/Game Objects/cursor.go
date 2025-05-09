a18915d0c01d140f6e05992a52cb338c3151ed5a
AM Sultanov <neurofag@gmail.com>
2025-05-09 07:27:36 +0300

    many fixes with ui etc

--- main/Game Objects/cursor.go
+++ main/Game Objects/cursor.go
@@ -2,19 +2,3 @@ components {
   id: "cursor"
   component: "/main/Scripts/cursor.script"
 }
-embedded_components {
-  id: "sprite"
-  type: "sprite"
-  data: "default_animation: \"2_blue\"\n"
-  "material: \"/builtins/materials/sprite.material\"\n"
-  "size {\n"
-  "  x: 10.0\n"
-  "  y: 10.0\n"
-  "}\n"
-  "size_mode: SIZE_MODE_MANUAL\n"
-  "textures {\n"
-  "  sampler: \"texture_sampler\"\n"
-  "  texture: \"/main/main.atlas\"\n"
-  "}\n"
-  ""
-}
